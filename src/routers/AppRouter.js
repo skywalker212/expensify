efda88528fa98fba7753f7066cc30f6af0844569
Akash Gajjar <akashgajjar8@gmail.com>
2018-05-27 19:09:16 +0530

    made some changes after setting up the boilerplate

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js'
+import AddExpensePage from '../components/AddExpensePage.js';
+import EditExpensePage from '../components/EditExpensePage.js';
+import Page404 from '../components/Page404.js';
+import Header from '../components/Header.js';
+import HelpPage from '../components/HelpPage.js';
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={ExpenseDashboardPage}/>
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit/:id" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={Page404} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
