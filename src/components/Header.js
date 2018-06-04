2fd6b4a1be1ea0db83d8815653165dee1c731141
Akash Gajjar <akashgajjar8@gmail.com>
2018-06-04 15:35:34 +0530

    added tests and travis.yml

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink exact activeClassName="is-active" to="/">Dashboard</NavLink>
         <NavLink activeClassName="is-active" to="/create">Create Expense</NavLink>
-        <NavLink activeClassName="is-active" to="/help">Help</NavLink>
     </header>
 );
 
