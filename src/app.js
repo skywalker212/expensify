eb3fcc9dbae51ed536125f5a7ac55fcd890f08a5
Akash Gajjar <akashgajjar8@gmail.com>
2018-06-05 01:50:14 +0530

    modified app.js to remove some unnessessary code

--- src/app.js
+++ src/app.js
@@ -12,12 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill',amount:4500}));
-store.dispatch(addExpense({description: 'gas bill',createdAt:1000}));
-store.dispatch(addExpense({description: 'rent',amount:109500}));
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
